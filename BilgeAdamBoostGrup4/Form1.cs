f5d5e2945f08da4126dd4344f9081c53b704d5c0
cangire <73399982+cangire@users.noreply.github.com>
2022-09-07 17:50:24 +0300

    labelle adımı ekledim

--- BilgeAdamBoostGrup4/Form1.cs
+++ BilgeAdamBoostGrup4/Form1.cs
@@ -16,5 +16,10 @@ namespace BilgeAdamBoostGrup4
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
