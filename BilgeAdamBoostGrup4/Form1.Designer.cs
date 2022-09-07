fae1dab8a2ae5e22a54ac0882ca7770af7f56183
tolga erçin <tolga.ercin.42@gmail.com>
2022-09-07 18:31:28 +0300

    deneme butonu eklemesi

--- BilgeAdamBoostGrup4/Form1.Designer.cs
+++ BilgeAdamBoostGrup4/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -60,11 +62,33 @@
             this.label3.TabIndex = 1;
             this.label3.Text = "tolga ";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.LiveSetting = System.Windows.Forms.Automation.AutomationLiveSetting.Polite;
+            this.label4.Location = new System.Drawing.Point(182, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "can";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(541, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 83);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "deneme butonu";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -79,6 +103,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
